4536002316bf2ed6418732afb40f84ec6bb57016
Himanshu-180875 <himanshupahuja55@gmail.com>
2023-09-16 23:43:06 +0530

    removed fuji config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,7 @@ module.exports = {
     localhost: {
       url: `http://127.0.0.1:8545/`,
       accounts: [`${process.env.LOCAL_PRIVATE_KEY}`],
-    },
-    fuji: {
-      url: `https://api.avax-test.network/ext/bc/C/rpc`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-    },
+    }
   },
   etherscan:{
     apiKey: `${process.env.ETHERSCAN_PRIVATE_KEY}`
